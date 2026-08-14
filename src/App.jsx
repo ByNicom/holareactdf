1897f104c0e6edd45b0b261a1fc6961e528c806d
Bynicom <nicolas.mandujano@usm.cl>
2026-08-13 21:13:23 -0400

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+// Functional Component
+
+function App() {
+ // let h1 = document.createElement(´h1´)
+ // h1.innerText = "Hola Mundo";
+ // return h1;
+  return <h1>Hola Mundo </h1>; 
+}
+
+export default App
